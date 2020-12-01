f45d9be6acaa8b8652f2bdcaa95c61df8d77599f
Mgilmore2020 <believe2@colostate.edu>
2020-12-01 15:58:23 -0700

    13.2 11/19 add router and components

---
+++ src/components/Workplace.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>Workplace</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/routes.js
@@ -0,0 +1,28 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+
+import Home from './components/Home.vue';
+import Account from './components/Account.vue';
+import ApplicationCreate from './components/ApplicationCreate.vue';
+import Login from './components/Login.vue';
+import NotFound from './components/NotFound.vue';
+import SignUp from './components/SignUp.vue';
+import Workplace from './components/Workplace.vue';
+import WorkplaceDetail from './components/WorkplaceDetail.vue';
+
+Vue.use(VueRouter);
+
+const routes = [
+    {path: '/', component: Home},
+    {path: '/account', component: Account},
+    {path: '/signin', component: Login},
+    {path: '/workplaces', component: Workplace},
+    {path: '/workplaces/:pk', component: WorkplaceDetail,
+        children: [
+            {path: '/application', component: ApplicationCreate}
+        ]},
+    {path: '/signup', component: SignUp},
+    {path: '/:invalidroute(.*)', component: NotFound}
+]
+
+export default new VueRouter({mode: 'history', routes})
