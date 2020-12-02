eec309a75c90d8999073567620c7fcaf42740e8b
Mgilmore2020 <believe2@colostate.edu>
2020-12-01 22:18:44 -0700

    13.2 11/19 axios signup route

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
         <h1>Log In</h1>
+        <div v-if="this.$route.query.signupsuccess" class="alert alert-success">Thank you for signing up! Please log in now.</div>
     </div>
 </template>
 
