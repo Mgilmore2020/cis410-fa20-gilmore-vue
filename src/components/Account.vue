21cfb82b30928589a5c1fd8818e853e769673f66
michaelford94 <mikeesmail1234@gmail.com>
2020-12-08 14:50:44 -0700

    account page updated

--- src/components/Account.vue
+++ src/components/Account.vue
@@ -1,14 +1,58 @@
 <template>
     <div>
         <h1>Account</h1>
+        <hr/>
+        <h3> {{firstName}}'s Applications </h3>
+
+        <p v-if="accountError" class="form-text text-danger">Can not get your account information, please try again later</p>
+        
+        <table v-if="reviewsByUser" class="table">
+            <thead>
+                <th>Title</th>
+                <th>Summary</th>
+                <th>Rating</th>
+            </thead>
+            <tbody>
+                <tr v-for="thisApplication in ApplicationsByUser" :key="thisApplication.ApplicationPK">
+                    <th><router-link :to="`/applications/${thisApplication.WorkplaceFK}`">{{thisApplication.About}}</router-link></th>
+                    <th>{{thisApplication.DateApplied}}</th>
+                    <!-- <th>{{thisApplication.Rating}}</th> -->
+                </tr>
+            </tbody>   
+        </table>
     </div>
 </template>
 
 <script>
+import axios from 'axios';
 export default {
-    
+    data(){
+        return{
+            applicationsByUser: null,
+            accountError: false
+        }
+    },
+    computed:{
+    firstName(){
+        console.log(this.$store.state)
+        return this.$store.state.user.NameFirst}
+    },
+    created(){
+        axios.get("/applications/me", {
+            headers: {
+                Authorization: `Bearer ${this.$store.state.token}`
+            }
+        })
+        .then((response)=>{ 
+            console.log("here is the applications/me response:", response)
+            this.applicationsByUser = response.data})
+        .catch(()=>{
+            this.accountError = true
+        })
+    }
 }
 </script>
+
 
 <style scoped>
 
