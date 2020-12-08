fb31213140d69ccf8aa1f13ec5ac49b13266afb2
Mgilmore2020 <believe2@colostate.edu>
2020-12-08 15:28:51 -0700

    Update

--- src/components/Account.vue
+++ src/components/Account.vue
@@ -1,12 +1,37 @@
 <template>
     <div>
         <h1>Account</h1>
+        <hr/>
+        <h3> {{firstName}}'s Applications </h3>
     </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
-    
+    data(){
+        return{
+            applicationsByUser: null,
+            accountError: false
+        }
+    },
+    computed:{
+        firstName(){
+            console.log(this.$store.state)
+            return this.$store.state.user.NameFirst}
+    },
+    created(){
+        axios.get("/application/me", {
+            headers:{
+                Authorization: `Bearer ${this.$store.state.token}`
+            }
+            .then((response)=>{ 
+                console.log(response)
+                console.log("Here is the application/me response", response)
+                this.applicationsByUser = response.data})
+        })
+    }
 }
 </script>
 
