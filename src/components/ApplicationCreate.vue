ad51033846c56778790dc3696162d630cb467a8a
michaelford94 <mikeesmail1234@gmail.com>
2020-12-08 14:36:10 -0700

    application create updated

--- src/components/ApplicationCreate.vue
+++ src/components/ApplicationCreate.vue
@@ -1,12 +1,63 @@
 <template>
-    <div>
-        <h1>Create Application</h1>
-    </div>
+     <div class="row justify-content-center">
+         <div class="col-lg-10">
+             <div class="card">
+                 <div class="card-body">
+                     <h4>Create an application</h4> 
+                     <form id="review-form" @submit.prevent="submitApplication">
+                        <div class="form-group">
+                             <label for="ratinginput">About</label> 
+                             <input type="number" id="ratinginput" name=" rating" min="1" max="10" required="required" class="form-control" v-model="about">
+                        </div> 
+                        <div class="form-group">
+                            <label for="summaryinput">Date Applied</label> <textarea row="3" id="summaryinput" name="summary" required="required" class="form-control" v-model="dateapplied"></textarea>
+                        </div> 
+                        
+                        <button type="submit" class="btn btn-primary">Submit Application</button> 
+                        
+                        <button v-on:click="cancelReview" type="clear" class="btn btn-outline-danger">
+                        Cancel
+                        </button>
+
+                        <p v-if="errorMessage" class="form-text text-danger">{{errorMessage}}</p>
+                    </form>
+                </div>
+        </div>
+        </div>
+        </div>
 </template>
 
 <script>
+import axios from 'axios';
 export default {
-    
+    data(){
+        return{
+            about: null,
+            dateapplied:"",
+            errorMessage: null,
+        }
+    },
+    methods:{
+        submitReview(){
+            const myReview={
+                about: this.about,
+                dateApplied: this.dateApplied,
+                workplaceFK: this.$route.params.fk
+            };
+            // console.log("here is the review", myReview)
+            const token = this.$store.state.token;
+            axios.post("/applications", myapplication, {
+                headers:{
+                    Authorization: `Bearer ${token}`
+                }
+            })
+            .then(()=>{this.$router.replace('/account')})
+            .catch(()=>{this.errorMessage = "Unable to create a review, please try again later."})
+        },
+        cancelReview(){
+            this.$router.go(-1)
+        }
+    }
 }
 </script>
 
